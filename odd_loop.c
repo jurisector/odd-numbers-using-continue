980065a2b19818523ac7f2a4fda0eb2851d0d14c
jurisector <szuveriya4@gmail.com>
2026-06-19 01:00:57 +0530

    Add files via upload

---
+++ odd_loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h> // checking if number is  even-odd  by using loop control term continue
+int main(){
+    int n;
+    printf("enter n"); // enter the number in terminal
+    scanff("%d",&n); // terminal save the number we input
+  for(int i=1 ; i<=n; i++){ // using for loop
+    if(i %2==0){continue;} // if the answer is zero number is even
+  
+   
+  printf("%d\n",i);} //the number  print in terminal 
+  // Thanks if any help u require jurisector is available ss
+  return 0;
+
+
+} 
+
